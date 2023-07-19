ee0106e62c988a52ee0ff6d7ab53acd9bebba486
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-07-18 22:39:27 -0400

    Agregue correos de robelin y mio

--- RedDeAmigos/Program.cs
+++ RedDeAmigos/Program.cs
@@ -6,10 +6,10 @@
         {
             ListaCircularDoble personas = new ListaCircularDoble();
 
-            personas.AgregarPorCola(new Persona("Alexander", "Suarez", 20, "809 546 1589", "alexander@gmial.com"));
-            personas.AgregarPorCola(new Persona("Alexander", "Suarez", 20, "809 546 1589", "alexander@gmial.com"));
+            personas.AgregarPorCola(new Persona("Alexander", "Suarez", 20, "809 546 1589", "alexander@gmail.com"));
+            personas.AgregarPorCola(new Persona("Julio", "Pichardo", 19, "829 767 5954", "juliopichardo@gmail.com"));
 
-            personas.AgregarOrdenado(new Persona("Bruno", "Suarez", 20, "809 546 1589", "Bruno@gmial.com"));
+            personas.AgregarOrdenado(new Persona("Robelin", "Concepcion", 17, "809 533 1389", "Robelin@gmail.com"));
 
             
 
