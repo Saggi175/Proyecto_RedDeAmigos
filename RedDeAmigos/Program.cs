236f7e346c1fe41c14975c86bc3bce72ea579a9d
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-07-18 22:09:19 -0400

    Linea17 puse en texto "Cantidad de personas en la red social"

--- RedDeAmigos/Program.cs
+++ RedDeAmigos/Program.cs
@@ -7,7 +7,7 @@
             ListaCircularDoble personas = new ListaCircularDoble();
 
             personas.AgregarPorCola(new Persona("Alexander", "Suarez", 20, "809 546 1589", "alexander@gmial.com"));
-            personas.AgregarPorCola(new Persona("Alexander", "Suarez", 20, "809 546 1589", "alexander01@gmial.com"));
+            personas.AgregarPorCola(new Persona("Alexander", "Suarez", 20, "809 546 1589", "alexander@gmial.com"));
 
             personas.AgregarOrdenado(new Persona("Bruno", "Suarez", 20, "809 546 1589", "Bruno@gmial.com"));
 
